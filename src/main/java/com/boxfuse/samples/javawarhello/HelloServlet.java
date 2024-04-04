8c709fc7bdb0c625e45f1c28ffcf839c316aad45
lakshmi13520 <lakshmi13520@gmail.cim>
2024-04-04 11:14:28 +0530

    scm change

--- src/main/java/com/boxfuse/samples/javawarhello/HelloServlet.java
+++ src/main/java/com/boxfuse/samples/javawarhello/HelloServlet.java
@@ -9,5 +9,6 @@ public class HelloServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         System.out.println("HelloServlet: You should see this in the Instance Logs :-)");
+        System.out.println("HelloServlet:: Lakshmi............");
     }
 }
